268e31275b00044d5273677c6eac0904c19a0f11
dev <dev@Philips-MBP.fritz.box>
2016-05-11 16:32:02 +0200

    Added accuracy metric and more training data

--- simple_svm.py
+++ simple_svm.py
@@ -1,11 +1,22 @@
-from sklearn import svm
-# Features
-X = [[0, 0], [1, 1]]
+import sys
+from class_vis import prettyPicture
+from prep_terrain_data import makeTerrainData
 
-# Labels
-y = [0, 1]
+import matplotlib.pyplot as plt
+import copy
+import numpy as np
+import pylab as pl
+from sklearn import svm
+from sklearn.metrics import accuracy_score
+
+features_train, labels_train, features_test, labels_test = makeTerrainData()
 
 clf = svm.SVC()
-clf.fit(X,y)
+clf.fit(features_train, labels_train)
 
-print clf.predict([[2., 2.]])
+predictions = clf.predict(features_test)
+
+# measure accuracy
+accuracy = accuracy_score(predictions, labels_test)
+print "Accuracy is: " + str(accuracy)
+
